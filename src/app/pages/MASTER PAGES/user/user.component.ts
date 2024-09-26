af66f7dc36b34674f4e322acb1f93c781ba46fc3
Nithin Sivadas <155439497+Arya-dinesh@users.noreply.github.com>
2024-09-26 11:23:20 +0530

    report page error cleared

--- src/app/pages/MASTER PAGES/user/user.component.ts
+++ src/app/pages/MASTER PAGES/user/user.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, NgModule, OnInit, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  NgModule,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import {
   DxDataGridModule,
   DxButtonModule,
@@ -14,68 +20,77 @@ import { FormPopupModule } from 'src/app/components';
 import notify from 'devextreme/ui/notify';
 import { ReportService } from 'src/app/services/Report-data.service';
 import { MasterReportService } from '../master-report.service';
-import { UserNewFormComponent,UserNewFormModule } from '../../POP-UP_PAGES/user-new-form/user-new-form.component';
-import { UserEditFormComponent,UserEditFormModule } from '../../POP-UP_PAGES/user-edit-form/user-edit-form.component';
+import {
+  UserNewFormComponent,
+  UserNewFormModule,
+} from '../../POP-UP_PAGES/user-new-form/user-new-form.component';
+import {
+  UserEditFormComponent,
+  UserEditFormModule,
+} from '../../POP-UP_PAGES/user-edit-form/user-edit-form.component';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
-  providers:[MasterReportService]
+  providers: [MasterReportService, ReportService],
 })
 export class UserComponent implements OnInit {
   @ViewChild(DxDataGridComponent, { static: true })
   dataGrid: DxDataGridComponent;
   @ViewChild(UserNewFormComponent, { static: false })
   userNewForm: UserNewFormComponent;
-  popupwidth:any='65%'
-  datasource:any;
+  popupwidth: any = '65%';
+  datasource: any;
 
-  isAddFormPopupOpened:boolean=false;
-  isEditPopupOpened:boolean=false;
-  selectedRowData:any;
+  isAddFormPopupOpened: boolean = false;
+  isEditPopupOpened: boolean = false;
+  selectedRowData: any;
 
-  constructor(private service:MasterReportService,private reportService:ReportService,private cdr: ChangeDetectorRef){}
+  constructor(
+    private service: MasterReportService,
+    private reportService: ReportService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   onEditingRow(event): void {
-    console.log(event,"event")
+    console.log(event, 'event');
     event.cancel = true;
     const Id = event.data.UserID;
-    console.log(Id,"id");
+    console.log(Id, 'id');
     this.isEditPopupOpened = true;
-    this.service.get_User_Data_By_Id(Id).subscribe(res=>{
+    this.service.get_User_Data_By_Id(Id).subscribe((res) => {
       this.selectedRowData = res;
       this.cdr.detectChanges(); // Ensure Angular picks up the change
-      console.log(this.selectedRowData)
-    })
-}
+      console.log(this.selectedRowData);
+    });
+  }
 
-  show_new_Form(){
+  show_new_Form() {
     this.isAddFormPopupOpened = true;
   }
-  getUSerData(){
-    this.service.get_User_data().subscribe(data=>{
-      this.datasource=data;
-      console.log('datasource',this.datasource);
-    })
+  getUSerData() {
+    this.service.get_User_data().subscribe((data) => {
+      this.datasource = data;
+      console.log('datasource', this.datasource);
+    });
   }
-  onClickSaveNewData(){
-    const data=this.userNewForm.getNewUserData();
-    console.log('inserted data',data);
-    this.service.insert_User_Data(data).subscribe((res:any)=>{
+  onClickSaveNewData() {
+    const data = this.userNewForm.getNewUserData();
+    console.log('inserted data', data);
+    this.service.insert_User_Data(data).subscribe((res: any) => {
       try {
-        if(res.message==='Success')
-        {
-        notify(
-          {
-            message: 'data saved successfully',
-            position: { at: 'top right', my: 'top right' },
-            displayTime: 500,
-          },
-          'success'
-        );
-        this.getUSerData();
-      }
+        if (res.message === 'Success') {
+          notify(
+            {
+              message: 'data saved successfully',
+              position: { at: 'top right', my: 'top right' },
+              displayTime: 500,
+            },
+            'success'
+          );
+          this.getUSerData();
+        }
       } catch (error) {
         notify(
           {
@@ -86,7 +101,7 @@ export class UserComponent implements OnInit {
           'error'
         );
       }
-    })
+    });
   }
 
   onRowRemoving(event: any) {
@@ -94,51 +109,47 @@ export class UserComponent implements OnInit {
     event.cancel = true;
     let SelectedRow = event.key;
     // console.log('selected row data :', SelectedRow);
-    this.service
-      .remove_User_Data(SelectedRow.UserID)
-      .subscribe(() => {
-        try {
-          notify(
-            {
-              message: 'Delete operation successful',
-              position: { at: 'top right', my: 'top right' },
-              displayTime: 500,
-            },
-            'success'
-          );
-          this.dataGrid.instance.refresh();
-        } catch (error) {
-          notify(
-            {
-              message: 'Delete operation failed',
-              position: { at: 'top right', my: 'top right' },
-              displayTime: 500,
-            },
-            'error'
-          );
-        }
-        event.component.refresh();
+    this.service.remove_User_Data(SelectedRow.UserID).subscribe(() => {
+      try {
+        notify(
+          {
+            message: 'Delete operation successful',
+            position: { at: 'top right', my: 'top right' },
+            displayTime: 500,
+          },
+          'success'
+        );
         this.dataGrid.instance.refresh();
-        this.getUSerData();
-      });
+      } catch (error) {
+        notify(
+          {
+            message: 'Delete operation failed',
+            position: { at: 'top right', my: 'top right' },
+            displayTime: 500,
+          },
+          'error'
+        );
+      }
+      event.component.refresh();
+      this.dataGrid.instance.refresh();
+      this.getUSerData();
+    });
   }
-
 
   //========================Export data ==========================
   onExporting(event: any) {
-    const fileName='Speciality'
-    this.reportService.exportDataGrid(event,fileName);
+    const fileName = 'Speciality';
+    this.reportService.exportDataGrid(event, fileName);
   }
 
-  CloseEditForm(){
-    this.isEditPopupOpened=false;
+  CloseEditForm() {
+    this.isEditPopupOpened = false;
     this.getUSerData();
   }
 
   ngOnInit(): void {
     this.getUSerData();
   }
-
 }
 @NgModule({
   imports: [
@@ -153,11 +164,10 @@ export class UserComponent implements OnInit {
     FormPopupModule,
     UserNewFormModule,
     DxPopupModule,
-    UserEditFormModule
+    UserEditFormModule,
   ],
   providers: [],
   exports: [],
   declarations: [UserComponent],
 })
-export class UserModule{}
-
+export class UserModule {}
